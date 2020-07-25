0f9c4442b1f603525b3f4f2cfe918dfc83bc7662
Rafael Vinueza <rafaelvinueza@gmail.com>
2020-07-25 16:55:42 -0500

    Terminadas las pantallas establecimiento e informacion del cupon
    primer diseño que hago, puede cambiar si algo esta mal

---
+++ app/src/main/java/com/moralesjuan/yourticketapp/InformacionDelCuponActivity.kt
@@ -0,0 +1,11 @@
+package com.moralesjuan.yourticketapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InformacionDelCuponActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informacion_del_cupon)
+    }
+}
